43c68c64f957345f895b6d2a8bdc7344e520ffb6
Pradhuman-singh5 <66350013+Pradhuman-singh5@users.noreply.github.com>
2021-05-06 17:34:32 +0530

    Add files via upload

---
+++ 100DayChallenge/src/com/codeing/SparseString.java
@@ -0,0 +1,24 @@
+package com.codeing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SparseString {
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+        // Write your code here
+        int counter =0;
+        List<Integer> q= new ArrayList<Integer>();
+        for(int i = 0;i<queries.size();i++){
+            for(int j=0;j<strings.size();j++){
+                if(queries.get(i).equals(strings.get(j))){
+                    counter= counter+1;
+                }
+                q.add(counter);
+            }
+            counter=0;
+        }
+
+        return q;
+    }
+}
+}
